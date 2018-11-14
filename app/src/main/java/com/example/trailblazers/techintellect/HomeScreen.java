375b04880b8c382473b044c3a9a4e4ffb8c23211
aravindg28 <aravindg@outlook.com>
2018-11-14 17:02:57 -0400

    Settings screen
    Committing settings screen UI

--- app/src/main/java/com/example/trailblazers/techintellect/HomeScreen.java
+++ app/src/main/java/com/example/trailblazers/techintellect/HomeScreen.java
@@ -1,13 +1,22 @@
 package com.example.trailblazers.techintellect;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeScreen extends AppCompatActivity implements Tab1.OnFragmentInteractionListener , Tab2.OnFragmentInteractionListener {
 
+    Boolean vibrateSwitchOn; //added by Aravind
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,4 +57,49 @@ public class HomeScreen extends AppCompatActivity implements Tab1.OnFragmentInte
     public void onFragmentInteraction(Uri uri) {
 
     }
+
+    //Added by Aravind for settings menu - Starts
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.settings_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.action_settings:
+                Intent settings = new Intent(this,SettingsActivity.class);
+                startActivityForResult(settings,1);
+                break;
+            case R.id.action_settings_logout:
+                break;
+            default:
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    //Fetches the switch value for vibration
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK) {
+                boolean switchVal = data.getBooleanExtra("switchVal",false);
+                vibrateSwitchOn = switchVal;
+                if(vibrateSwitchOn)
+                   Toast.makeText(getApplicationContext(),"Vibrate is on", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(),"Vibrate is off", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+    //Added by Aravind for settings menu - Ends
 }
