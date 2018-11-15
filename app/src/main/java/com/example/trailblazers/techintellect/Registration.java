1b16c237c94471f7142a0dc317766d6b35e34203
aravindg28 <aravindg@outlook.com>
2018-11-15 17:28:02 -0400

    Regsitration backend
    Committing registration backend logic

--- app/src/main/java/com/example/trailblazers/techintellect/Registration.java
+++ app/src/main/java/com/example/trailblazers/techintellect/Registration.java
@@ -1,7 +1,9 @@
 package com.example.trailblazers.techintellect;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.TextUtils;
@@ -11,6 +13,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
 /*************************************************************************************************************************************************************
  Program: Registration class handling validations and registration operations.
@@ -27,6 +35,8 @@ public class Registration extends AppCompatActivity {
     private EditText email;
     private EditText password;
     private EditText confirm_password;
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +46,8 @@ public class Registration extends AppCompatActivity {
     }
 
     private void initializeComponents() {
-
+        auth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
         full_name = findViewById(R.id.full_name_value);
         email = findViewById(R.id.email_address_value);
         password = findViewById(R.id.create_new_password);
@@ -47,9 +58,29 @@ public class Registration extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(validateEntries()){
-                    Toast.makeText(getApplicationContext(), "Success! Kindly login to proceed.", Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
+                    progressDialog.setMessage("Registering..Please wait!");
+                    progressDialog.show();
+                    Thread mthread = new Thread(){  //Creating a separate thread for progress dialog
+                        @Override
+                        public void run() {
+                            auth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        progressDialog.hide();
+                                        Toast.makeText(getApplicationContext(), "Success! Kindly login to proceed.", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    if(!task.isSuccessful()){
+                                        FirebaseAuthException e = (FirebaseAuthException )task.getException();
+                                        Toast.makeText(getApplicationContext(), "Failed Registration: "+e.getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        }
+                    };
+                  mthread.start();
                 }
 
             }
@@ -67,17 +98,17 @@ public class Registration extends AppCompatActivity {
             }
         });
     }
-
+    //Checks if the field is empty or not
     boolean isEmpty(EditText text){
        CharSequence input = text.getText().toString();
        return TextUtils.isEmpty(input);
     }
-
+   //Checks if the email is valid or not
     boolean isEmail(EditText text){
         CharSequence input = text.getText().toString();
         return (!TextUtils.isEmpty(input) && Patterns.EMAIL_ADDRESS.matcher(input).matches());
     }
-
+    //Method for validating user inputs
     private boolean validateEntries(){
         if(isEmpty(full_name)){
             full_name.setError("Full name is required");
@@ -93,6 +124,10 @@ public class Registration extends AppCompatActivity {
         }
         if(isEmpty(confirm_password)){
             Toast.makeText(getApplicationContext(), "Please confirm your password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(password.getText().toString().length() < 6 || confirm_password.getText().toString().length() < 6){
+            Toast.makeText(getApplicationContext(), "Password should be at least 6 characters", Toast.LENGTH_SHORT).show();
             return false;
         }
         if(!password.getText().toString().equals(confirm_password.getText().toString())){
