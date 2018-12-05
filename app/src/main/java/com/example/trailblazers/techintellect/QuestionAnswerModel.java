48de8753fbd9607907fd5dd93e83ceaa8b1894e5
aravindg28 <aravindg@outlook.com>
2018-12-05 19:45:42 -0400

    dashboard changes by smit

--- app/src/main/java/com/example/trailblazers/techintellect/QuestionAnswerModel.java
+++ app/src/main/java/com/example/trailblazers/techintellect/QuestionAnswerModel.java
@@ -2,11 +2,27 @@ package com.example.trailblazers.techintellect;
 
 public class QuestionAnswerModel {
     private String Question;
-    private String Answer;
+    private String Correct_answer;
+    private String Answer1;
+    private String Answer2;
+    private String Answer3;
+    private String Answer4;
 
-    public QuestionAnswerModel(String question, String answer) {
+
+    public QuestionAnswerModel() {
+    }
+    public QuestionAnswerModel(String question, String correct_answer){
         Question = question;
-        Answer = answer;
+        Correct_answer = correct_answer;
+    }
+    public QuestionAnswerModel(String question, String answer, String answer1,String answer2,String answer3,String answer4) {
+        Question = question;
+        Correct_answer = answer;
+        Answer1 = answer1;
+        Answer2 = answer2;
+        Answer3 = answer3;
+        Answer4 = answer4;
+
     }
 
     public String getQuestion() {
@@ -18,10 +34,51 @@ public class QuestionAnswerModel {
     }
 
     public String getAnswer() {
-        return Answer;
+        return Correct_answer;
     }
 
     public void setAnswer(String answer) {
-        Answer = answer;
+        Correct_answer = answer;
+    }
+
+    public String getAnswer1() {
+        return Answer1;
+    }
+
+    public void setAnswer1(String answer1) {
+        Answer1 = answer1;
+    }
+
+    public String getCorrect_answer() {
+        return Correct_answer;
+    }
+
+    public void setCorrect_answer(String correct_answer) {
+        Correct_answer = correct_answer;
+    }
+
+    public String getAnswer2() {
+        return Answer2;
+
+    }
+
+    public void setAnswer2(String answer2) {
+        Answer2 = answer2;
+    }
+
+    public String getAnswer3() {
+        return Answer3;
+    }
+
+    public void setAnswer3(String answer3) {
+        Answer3 = answer3;
+    }
+
+    public String getAnswer4() {
+        return Answer4;
+    }
+
+    public void setAnswer4(String answer4) {
+        Answer4 = answer4;
     }
 }
